260873900855f5cf0e6c32e25a0d4cf0733eac28
Frederic Koehler <fkfire@gmail.com>
2009-06-29 20:57:09 -0400

    Add streq2 test

--- tests/test_util.c
+++ tests/test_util.c
@@ -28,8 +28,24 @@ void test_isabsolute() {
 	     ++t) assert_false(isabsolute(*t));
 }
 
+void test_streq2() {
+#define TRUE(y, x1, x2) assert_true(streq2(y, x1, x2))
+#define FALSE(y, x1, x2) assert_false(streq2(y, x1, x2))
+	TRUE("", "", "");
+	TRUE(" ", " ", "");
+	TRUE("test_true", "test", "_true");
+	TRUE("test_true", "test_true", "");
+	FALSE("", " ", "");
+	FALSE("test_false", "test", "_fals");
+	FALSE("test_false", "test", "_falsee");
+	FALSE("test_false", "test", "_falsx");
+#undef TRUE
+#undef FALSE
+}
+
 TestSuite *util_tests() {
     TestSuite *suite = create_test_suite();
     add_test(suite, test_isabsolute);
+    add_test(suite, test_streq2);
     return suite;
 }
