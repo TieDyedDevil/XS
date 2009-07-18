33d9442631d0355c79f041a394252f6b6336677b
Frederic Koehler <fkfire@gmail.com>
2009-07-17 20:48:45 -0400

    More work on gc_ptr - implicit constructors, etc.

--- gc_ptr.hxx
+++ gc_ptr.hxx
@@ -1,29 +1,63 @@
 /* Reference on Stack */
 template <class T>
 class SRef {
-	public:
+	public: 
+		SRef() : ref(NULL) {}
 		SRef(const SRef<T>& orig) : ref(orig.ref) {
-			root.p = &ref;
-			root.next = rootlist;
-			rootlist = root;
+			addroot();
 		}
 		SRef(T* x) : ref(x) {
-			root.p = &ref;
-			root.next = rootlist;
-			rootlist = root;
+			addroot();
+		}
+		SRef& operator=(const SRef& orig) {
+			unroot();
+			ref = orig.ref;
+			addroot();
+		}
+		SRef& operator=(T* x) {
+			unroot();
+			ref = x;
+			addroot();
 		}
 		~SRef() {
-			refassert(rootlist == &root); \
-			refassert(rootlist->p == (void **) &ref); \
-			rootlist = rootlist->next;
+			unroot();
+		}
+		T* get() {
+			assert(gcisblocked());
+			return ref;
+		}
+		/* unchecked version of get. Ocasionally there are valid reasons
+		 * that the gc would be enabled, for example if assigning
+		 * into a scanned type
+		 */
+		T* uget() {
+			return ref;
+		}
+		T* release() {
+			unroot();
+			return ref;
 		}
 		T& operator*() {
+			assert (ref != NULL);
 			return *ref;
 		}
 		T* operator->() {
+			assert (ref != NULL);
 			return ref;
 		}
 	private:
+		void addroot() {
+			root.p = reinterpret_cast<void**>(&ref);
+			root.next = rootlist;
+			rootlist = &root;
+		}
+		void unroot() {
+			if (ref == NULL) return;
+			refassert(rootlist == &root); \
+			refassert(rootlist->p == reinterpret_cast<void **>(&ref)); \
+			rootlist = rootlist->next;
+			ref = NULL;
+		}
 		Root root;
 		T *ref;
 };
