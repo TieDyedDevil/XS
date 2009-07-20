f887bb9ca1937be192d967f8fd1ff3d5749c4c35
Frederic Koehler <fkfire@gmail.com>
2009-07-20 04:28:45 -0400

    Try to explain SRef usage in comments

--- gc_ptr.hxx
+++ gc_ptr.hxx
@@ -1,3 +1,16 @@
+/* Stack/Paramater/Return type/etc. reference
+   Should be used for gc references which cannot be auto-forwarded
+   otherwise (i.e. are not pointers contained within another reference),
+   otherwise the reference may either
+   		* be considered unused and deleted
+		* or be forwarded without updating your pointer
+	both leading to bad things
+   Works fine with non-gc pointers too (although it doesn't auto-free them)
+
+   You should NOT, as an exception, form an SRef to an object with a custom scanner/copier
+   while that scanner/copier would not operate correctly, if the object you are creating
+   will not be setup properly by the next possible gc run (gc(), gcenable(), or any gc allocation)
+*/
 template <class T>
 class SRef {
 #include "gc_ptr1.hxx"
@@ -10,6 +23,7 @@ class SRef {
 			assert(ref != NULL);
 			return ref[n];
 		}
+		
 };
 
 template<>
