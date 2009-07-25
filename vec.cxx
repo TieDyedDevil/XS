2ac1fc650876beb1cdecbd2c2b969c0bf7ef264b
Frederic Koehler <fkfire@gmail.com>
2009-07-25 19:12:41 -0400

    Fix, cleanup in vec.cxx: 	* Bugfix: Use temporary to hold result of gcdup() in vectorize 		to avoid forwarding of the left-side of expression

--- vec.cxx
+++ vec.cxx
@@ -2,16 +2,19 @@
 
 #include "es.hxx"
 #include "gc.hxx"
+#include <iostream>
 #include <algorithm>
 
 DefineTag(Vector, static);
 
+size_t vector_size(const Vector *v) {
+	/* TODO: +1 is probably just an artifact of an earlier time, no? */
+	return reinterpret_cast<const char*>(v->vector + (v->alloclen + 1)) - reinterpret_cast<const char*>(v);
+}
+
 extern SRef<Vector> mkvector(int n) {
 	int i;
-	SRef<Vector> v = reinterpret_cast<Vector*>(
-	  gcalloc(offsetof(Vector, vector[0]) +
-		  (n + 1) * sizeof(char*),
-		  &VectorTag));
+	SRef<Vector> v = reinterpret_cast<Vector*>(gcalloc(offsetof(Vector, vector[0]), &VectorTag));
 	v->alloclen = n;
 	v->count = 0;
 	for (i = 0; i <= n; i++)
@@ -19,9 +22,9 @@ extern SRef<Vector> mkvector(int n) {
 	return v;
 }
 
-static void *VectorCopy(void *ov) {
-	size_t n = offsetof(Vector, vector[0]) + sizeof(char*) *
-	  (reinterpret_cast<Vector *>(ov)->alloclen + 1);
+static void *VectorCopy(void *ov_void) {
+	Vector *ov = reinterpret_cast<Vector*>(ov_void);
+	size_t n = vector_size(ov);;
 	void *nv = gcalloc(n, &VectorTag);
 	memcpy(nv, ov, n);
 	return nv;
@@ -29,27 +32,24 @@ static void *VectorCopy(void *ov) {
 
 static size_t VectorScan(void *p) {
 	Vector *v = reinterpret_cast<Vector*>(p);
-	int i, n = v->count;
-	for (i = 0; i <= n; i++)
+	for (int i = 0; i <= v->count; i++)
 		v->vector[i] = reinterpret_cast<char*>(forward(v->vector[i]));
-	return offsetof(Vector, vector[0]) + sizeof(char*) * (v->alloclen + 1);
+	return vector_size(v);;
 }
 
-
 extern SRef<Vector> vectorize(SRef<List> list) {
-	int i, n = length(list.uget());
-
-	SRef<Vector> v = NULL;
-	v = mkvector(n);
-	v->count = n;
-
-	for (i = 0; list != NULL; list = list->next, i++) {
-	        /* must evaluate before v->vector[i] */
-		SRef<const char> s = getstr(list->term); 
-		v->vector[i] = gcdup(s.uget());
+	int n = length(list.uget());
+	SRef<Vector> v = mkvector(n);
+	v->count = n; /* GC may forward extra NULL pointers, no matter */
+	for (int i = 0; list != NULL; list = list->next, i++) {
+		/* gcdup() must occur _before_ v->vector[i] because
+		 * otherwise v may be forwarded
+		 */
+		char *t = gcdup(getstr(list->term));
+		v->vector[i] = t;
 	}
 
-	return v.release();
+	return v;
 }
 
 /* qstrcmp -- a strcmp wrapper for sort */
