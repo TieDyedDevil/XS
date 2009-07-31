229d34e5680024a1e6587868424a15681443a385
Frederic Koehler <fkfire@gmail.com>
2009-07-31 19:24:08 -0400

    Change version string to 1.0

--- version.cxx
+++ version.cxx
@@ -1,4 +1,4 @@
 /* version.c -- version number ($Revision: 1.2 $) */
 #include "es.hxx"
-static const char id[] = "@(#)xs version 0.1-prerelease";
+static const char id[] = "@(#)xs version 1.0-July 31 2009";
 const char * const version = id + (sizeof "@(#)" - 1);
