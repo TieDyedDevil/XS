dd38385321e737693fc66fef5536a6046cf32c41
frytvm <frytvm@arch.localdomain>
2009-10-30 20:33:36 -0400

    Minor cleanup

--- tree.cxx
+++ tree.cxx
@@ -2,8 +2,9 @@
 
 #include "es.hxx"
 
-static Tree* tgalloc(size_t s) {
-	return reinterpret_cast<Tree*>(galloc(s));
+template <int size>
+static Tree* newtree() {
+	return reinterpret_cast<Tree*>(galloc(offsetof(Tree, u[size])));
 }
 
 /* mk -- make a new node; used to generate the parse tree */
@@ -16,34 +17,33 @@ extern Tree *mk (NodeKind  t, ...) {
 	    default:
 		panic("mk: bad node kind %d", t);
 	    case nWord: case nQword: case nPrim:
-		n = tgalloc(offsetof(Tree, u[1]));
+		n = newtree<1>();
 		n->u[0].s = va_arg(ap, char *);
 		break;
 	    case nCall: case nThunk: case nVar:
-		n = tgalloc(offsetof(Tree, u[1]));
+		n = newtree<1>();
 		n->u[0].p = va_arg(ap, Tree *);
 		break;
 	    case nAssign:  case nConcat: case nClosure: case nFor:
 	    case nLambda: case nLet: case nList:  case nLocal:
 	    case nVarsub: case nMatch: case nExtract:
-		n = tgalloc(offsetof(Tree, u[2]));
+		n = newtree<2>();
 		n->u[0].p = va_arg(ap, Tree *);
 		n->u[1].p = va_arg(ap, Tree *);
 		break;
 	    case nRedir:
-		n = tgalloc(offsetof(Tree, u[2]));
+		n = newtree<2>();
 		n->u[0].p = va_arg(ap, Tree *);
 		n->u[1].p = va_arg(ap, Tree *);
 		break;
 	    case nPipe:
-		n = tgalloc(offsetof(Tree, u[2]));
+		n = newtree<2>();
 		n->u[0].i = va_arg(ap, int);
 		n->u[1].i = va_arg(ap, int);
 		break;
  	}
 	n->kind = t;
 	va_end(ap);
-
 	
 	return n;
 }
