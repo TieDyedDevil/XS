054bce85d6f51471fbbba7600b2caaba05e73b13
David B. Lamkins <david@lamkins.net>
2017-03-11 08:15:28 -0800

    Include package URL in version.

--- version.cxx
+++ version.cxx
@@ -1,4 +1,4 @@
 /* version.c -- version number */
 #include "xs.hxx"
-static const char id[] = PACKAGE_STRING;
+static const char id[] = PACKAGE_STRING " " PACKAGE_URL;
 const char * const version = id;
