95920f50fcc698ee236ff3a44b4f0f8b199a923a
David B. Lamkins <david@lamkins.net>
2017-04-16 00:31:13 -0700

    &cmp now handles numeric comparisons.

--- prim-rel.cxx
+++ prim-rel.cxx
@@ -3,13 +3,29 @@
 #include "xs.hxx"
 #include "prim.hxx"
 #include <string.h>
+#include <stdlib.h>
+
+static int isnumber(const char *s) {
+	return strspn(s, "0123456789.") == strlen(s);
+}
+
+static int isfloat(const char *s) {
+	return strchr(s, '.') != NULL;
+}
 
 PRIM(cmp) {
 	if (list == NULL || list->next == NULL)
 		fail("$&cmp", "usage: $&cmp a b");
 	const char *a = getstr(list->term);
 	const char *b = getstr(list->next->term);
-	int r = strcoll(a, b);
+	long r;
+	if (isnumber(a) && isnumber(b)) {
+		if (isfloat(a) || isfloat(b)) {
+			float va = strtod(a, NULL);
+			float vb = strtod(b, NULL);
+			r = va > vb ? 1 : va < vb ? -1 : 0;
+		} else r = strtol(a, NULL, 10) - strtol(b, NULL, 10);
+	} else r = strcoll(a, b);
 
 	list = mklist(mkstr(str("%d", r > 0 ? 1 : r < 0 ? -1 : 0)), NULL);
 	return list;
