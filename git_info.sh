1ae2c17ee36aeb15f6e6c1078139ebe34f08d7ed
David B. Lamkins <david@lamkins.net>
2017-04-30 08:59:23 -0700

    Tag git info string. Remove spurious flag.

--- git_info.sh
+++ git_info.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env sh
 
-git describe --abbrev=7 --dirty --always --tags | sed 's/^/#define GIT_INFO "/' | sed 's/$/"/'
+git describe --dirty --always --tags | sed 's/^/#define GIT_INFO "git: /' | sed 's/$/"/'
