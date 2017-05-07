3c14fa38d4b5ce76bf8249d1424ec2033bc19a8d
David B. Lamkins <david@lamkins.net>
2017-05-07 10:19:31 -0700

    Canonicalize git URL.

--- generators/git_url.sh
+++ generators/git_url.sh
@@ -2,6 +2,6 @@
 
 [ -n "$MESON_SOURCE_ROOT" ] && cd $MESON_SOURCE_ROOT
 FILE=$(basename $0 .sh).hxx
-DATA=$(git remote get-url origin |
+DATA=$(git remote get-url origin | sed 's/\.git$//' |
 	sed 's/^/#define GIT_URL "/' | sed 's/$/"/')
 echo $DATA | cmp -s - $FILE || echo "$DATA" > $FILE
