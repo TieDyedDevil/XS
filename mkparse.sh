afd9abe16417bb1d23f70f28087067a498742632
David B. Lamkins <david@lamkins.net>
2017-05-06 18:32:02 -0700

    Remove more autotools machinery.

--- mkparse.sh
+++ mkparse.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env sh
 
-../ylwrap ../parse.yxx y.tab.c parse.cxx y.tab.h `echo parse.cxx | sed -e s/cc$/hh/ -e s/cpp$/hpp/ -e s/cxx$/hxx/ -e s/c++$/h++/ -e s/c$/h/` y.output parse.output -- bison -y -d 
-
+bison -y -d ../parse.yxx
+mv y.tab.c parse.cxx
+mv y.tab.h parse.hxx
