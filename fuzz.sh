524454442ece712d43fa7cbab32f2c6abcc83d83
David B. Lamkins <david@lamkins.net>
2017-05-13 22:06:58 -0700

    This should make the parser work a bit harder during fuzzing by avoiding the unexpected NUL check.

--- fuzz.sh
+++ fuzz.sh
@@ -3,5 +3,5 @@
 TESTFILE=`mktemp`
 cat ./initial.xs >$TESTFILE
 find ./xs_tests -name '*.xs'|xargs cat >>$TESTFILE
-zzuf -O copy -s 1:10000 -r 0.001:0.050 -vic ./build/xs -p -n $TESTFILE
+zzuf -O copy -s 1:10000 -r 0.001:0.050 -R '\000' -vic ./build/xs -p -n $TESTFILE
 rm $TESTFILE
