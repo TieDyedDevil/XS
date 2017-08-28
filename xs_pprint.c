235a87c6cf02d9f3b4853b57269d2ca3bb491137
David B. Lamkins <david@lamkins.net>
2017-08-28 09:40:03 -0700

    xs_pprint: Add line continuations. Emit words present as args.

--- xs_pprint.c
+++ xs_pprint.c
@@ -3,9 +3,15 @@
 /* Trivial prettyprinter for canonical-form xs programs */
 #define INDENT 4
 int main(int argc, char *argv[]) {
-	unsigned ch, depth = 0;
+	int i;
+	for (i = 1; i < argc; ++i) {
+		fputs(argv[i], stdout);
+		putchar(' ');
+	}
+	unsigned ch, depth = argc == 1 ? 0 : 1;
 	while ((ch = getchar()) != EOF) {
 		if (ch == '{') {
+			if (depth) putchar('\\');
 			putchar('\n');
 			int i;
 			for (i = 0; i < INDENT*depth; ++i)
