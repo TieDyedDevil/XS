7aeaae287788a27243735059653b1a5becd3f5fa
David B. Lamkins <david@lamkins.net>
2017-09-20 18:17:10 -0700

    Automagically recover from meson version skew.

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #! /usr/bin/env sh
 
+touch .build
 [ -d build ] || meson build
 ninja -C build "$@"
+if [ $? -ne 0 ] && [ .build -nt build ]; then
+	rm -rf build
+	meson build
+	ninja -C build "$@"
+fi
+rm -f .build
