5ce63e44f5a87306f2408fab2ee13db24ed0309c
David B. Lamkins <david@lamkins.net>
2017-09-23 10:26:01 -0700

    Explicitly support both GCC and Clang.

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/env sh
 
+if [ "$1" = '--clang' ]; then
+	export CC=clang
+	export CXX=clang++
+	shift
+	rm -rf build
+elif [ "$1" = '--gcc' ]; then
+	export CC=gcc
+	export CXX=g++
+	shift
+	rm -rf build
+fi
 touch .build
 [ -d build ] || meson build
 ninja -C build "$@"
