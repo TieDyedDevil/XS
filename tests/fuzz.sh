871aa2977f6848b157eb0b49e54ab7fbf98bf691
David B. Lamkins <david@lamkins.net>
2017-09-26 17:08:36 -0700

    Restore and rework fuzzer test.

--- tests/fuzz.sh
+++ tests/fuzz.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env sh
 
 TESTFILE=`mktemp`
-cat ./initial.xs >$TESTFILE
-find ./xs_tests -name '*.xs'|xargs cat >>$TESTFILE
-zzuf -O copy -s 1:10000 -r 0.001:0.050 -R '\000' -vic ./build/xs -p -n $TESTFILE
-rm $TESTFILE
+find . -name '*.xs'|xargs cat >>$TESTFILE
+zzuf -O copy -s 1:100000 -r 0.0001:0.0050 -R '\000' -vic ./build/xs -p -n $TESTFILE
+rm -f $TESTFILE
