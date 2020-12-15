a0c2bf843953866df934125faa886052c9cd5c93
David B. Lamkins <david@lamkins.net>
2020-12-14 23:42:18 -0800

    $&build also describes the compiler.

--- generators/buildinfo.sh
+++ generators/buildinfo.sh
@@ -2,5 +2,6 @@
 
 [ -n "$MESON_SOURCE_ROOT" ] && cd $MESON_SOURCE_ROOT
 FILE=$(basename $0 .sh).hxx
-DATA="#define BUILDINFO \"$USER @ `hostname`; `date`\""
+COMPILER=$(${CC:-cc} --version|head -1)
+DATA="#define BUILDINFO \"$USER @ `hostname`; `date --rfc-3339=s`; $COMPILER\""
 echo "$DATA" | cmp -s - $FILE || echo "$DATA" > $FILE
